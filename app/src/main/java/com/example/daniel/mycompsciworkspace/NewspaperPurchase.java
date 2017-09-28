b9edd9f103b3c1c522cc4b5acc9a6be5ce3b947c
DanWilliams24 <01danielwilliams@gmail.com>
2017-09-28 06:03:30 -0400

    4 problem Solutions added

---
+++ app/src/main/java/com/example/daniel/mycompsciworkspace/NewspaperPurchase.java
@@ -0,0 +1,68 @@
+package com.example.daniel.mycompsciworkspace;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * Created by tina on 9/18/17.
+ */
+
+public class NewspaperPurchase {
+    public static void main(String[] args){
+        ArrayList<String> namesArray = new ArrayList<String>();
+        Double total = 0.00;
+        Integer itemNums = 0;
+        String userItemCode;
+        Double cost = 0.00;
+        Double average = 0.00;
+        Boolean isDone = false;
+        String name = "";
+        String moreDataInput;
+
+        while (isDone != true){
+            System.out.println("Enter Newspaper Name and cost in  the following format(Newspaper-0.00): ");
+            Scanner reader = new Scanner(System.in);
+            userItemCode = reader.next();
+
+            for(int i = 0; i < userItemCode.length(); i++){
+                Character currentChar = userItemCode.charAt(i);
+                if(currentChar.equals('-')){
+                    Integer dashLocation = i;
+                    name = " ";
+                    for(int x = 0; x < dashLocation; x++){
+                        name = name+ userItemCode.charAt(x);
+
+                    }
+                    namesArray.add(name);
+
+                    String price = " "+ userItemCode.charAt(dashLocation + 1)
+                                      + userItemCode.charAt(dashLocation +2)
+                                      + userItemCode.charAt(dashLocation +3)
+                                      + userItemCode.charAt(dashLocation +4);
+                    cost = Double.parseDouble(price);
+
+                }
+            }
+
+            total = total + cost;
+            itemNums = itemNums +1;
+            System.out.println(name);
+            System.out.println(cost);
+
+            System.out.println("Add more data?(y/n)");
+            moreDataInput = reader.next();
+            if(moreDataInput.equals("n")){
+                isDone = true;
+            }
+        }
+        DecimalFormat numberFormatter = new DecimalFormat("#.00");
+
+        average  = total/itemNums;
+        System.out.println("Total: $"+ numberFormatter.format(total));
+        System.out.println("Average: $"+ numberFormatter.format(average));
+
+        System.out.println(namesArray);
+
+    }
+}
---
+++ app/src/main/java/com/example/daniel/mycompsciworkspace/WordAndCharCount.java
@@ -0,0 +1,52 @@
+package com.example.daniel.mycompsciworkspace;
+
+
+
+
+import java.util.Scanner;
+
+/**
+ * Created by tina on 9/17/17.
+ */
+
+public class WordAndCharCount {
+    public static void main(String[] args) {
+
+        Character ch;
+        Integer wordCount = 0;
+        Integer sentenceCount = 0;
+        Integer charCount = 0;
+
+        System.out.println("Enter Text");
+        Scanner reader = new Scanner(System.in);
+        String text = "My name is Daniel Williams. I love computer programming,technology and the sciences. I am passionate about coding because it allows me to express my creativity. I took this class because as a programmer I need to create functional & beautiful experiences for my users to experience. By taking this class, I hope to further my artistic knowledge and learn to recognize art and its history.";
+
+        for(int i = 0; i < text.length(); i++){
+            ch = text.charAt(i);
+            if(ch.equals('.')){
+
+                charCount = charCount+ 1; //New
+                sentenceCount = sentenceCount +1;
+                System.out.print(ch);
+            } else if(ch.equals(' ')){
+                wordCount = wordCount +1;
+                charCount = charCount +1;
+                System.out.print(ch);
+            } else {
+                charCount = charCount +1;
+                System.out.print(ch);
+            }
+
+        }
+        System.out.println();
+        System.out.println("Word Count: "+ wordCount);
+        System.out.println("Character Count: "+charCount);
+        System.out.println("Sentence Count: "+sentenceCount);
+
+
+
+
+    }
+
+
+}
